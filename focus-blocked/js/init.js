dbad332cbca5b246dbd8eb37870101092999d51d
Amanuel Tesfaye <amanueltsfy@users.noreply.github.com>
2024-09-23 09:01:00 +0300

    fix: issue 265 resolved (#266)
    * fix: issue 263 resolved
    
    * fix: issue 265 resolved

--- focus-blocked/js/init.js
+++ focus-blocked/js/init.js
@@ -40,7 +40,7 @@ const old_url = current_url.substring(
     ? current_url.length
     : current_url.indexOf('?')
 );
-const domain = new URL(old_url).hostname;
+const domain = new URL(decodeURIComponent(old_url)).hostname;
 const focus_mode = urlParams.get('focus_mode') ?? null;
 const block_type = urlParams.get('block_type') ?? null;
 const cuddly_bear_mode = urlParams.get('cuddly_bear_mode') === 'true';
